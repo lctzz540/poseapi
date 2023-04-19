d52bbeb99016d1fd5bfdd1ea93bb9810a76da662
Thang Le <lctzz540@outlook.com>
2023-04-19 12:03:41 +0700

    edited

--- testapi.py
+++ testapi.py
@@ -9,7 +9,7 @@ with open("../Tree Pose_Right.csv", "rb") as f:
 
 # Create the request payload with the file content
 files = {"file": ("test_data.csv", file_content)}
-
+print("test_data.csv", file_content)
 # Send the POST request to the API endpoint with the file upload
 response = requests.post(url, files=files)
 
